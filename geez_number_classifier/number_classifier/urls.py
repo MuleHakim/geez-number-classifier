75705e812d531e1d3b8e9871c7e83b2caaca06a6
MuleHakim <mulukenhakim97@gmail.com>
2023-01-17 13:58:02 +0300

    machine learning backend updated using django

--- geez_number_classifier/number_classifier/urls.py
+++ geez_number_classifier/number_classifier/urls.py
@@ -1,1 +1,11 @@
-#
+from django.urls import path,include
+from rest_framework import routers
+from .views import ImageViewset
+from .views import PredictViewset
+router = routers.DefaultRouter()
+router.register(r'numbers',ImageViewset)
+urlpatterns = [
+    path('',include(router.urls)),
+    path('api-auth/',include('rest_framework.urls',namespace='rest_framework')),
+    path('predict/',PredictViewset,name='predict'),
+]

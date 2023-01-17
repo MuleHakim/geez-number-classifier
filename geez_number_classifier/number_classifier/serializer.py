d5f6b8367d26deefa7c5cc5187a2618ae4a712b6
MuleHakim <mulukenhakim97@gmail.com>
2023-01-17 13:59:21 +0300

    machine learning backend updated using django

--- geez_number_classifier/number_classifier/serializer.py
+++ geez_number_classifier/number_classifier/serializer.py
@@ -1,1 +1,8 @@
-#
+from .models import NumbersImage
+from rest_framework import serializers
+
+class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NumbersImage
+        fields = ["id","title","image_url","creation_date"]
+        

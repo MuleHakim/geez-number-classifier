a6c5e55f3f2e16ca769be3daaafe1360c0ad3173
Segni Dessalegn <segnides@gmail.com>
2023-01-18 16:31:42 +0300

    utils

---
+++ geez_number_classifier_flutter/lib/utils.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  late String text;
+  late Function func;
+  Button(this.text, this.func, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      margin: const EdgeInsets.all(10),
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(color: Colors.blue)),
+      child: TextButton(
+          onPressed: () => func(),
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 25),
+          )),
+    );
+  }
+}
